6efbba4bd58d81c35fbe0db68ccce8a7e956f80e
yoannspace <yoann.moranville@gmail.com>
2020-12-07 11:43:46 +0100

    Fix tests

--- src/test/java/eu/dariah/ToolXtractor/DHSAXParserTest.java
+++ src/test/java/eu/dariah/ToolXtractor/DHSAXParserTest.java
@@ -13,9 +13,11 @@ public class DHSAXParserTest {
     @Test
     public void testIgnoreCase() throws IOException, SAXException, ParserConfigurationException {
         DHSAXParser dhsaxParser = new DHSAXParser();
-        OutputStream outputStream = new FileOutputStream("src/test/resources/data/annotated_stax_3.xml");
+        File outputXmlFile = new File("src/test/resources/data/annotated_stax_3.xml");
+        OutputStream outputStream = new FileOutputStream(outputXmlFile);
         dhsaxParser.rewriteXml(new FileInputStream("src/test/resources/data/3.xml"),
                 new OutputStreamWriter(outputStream), Collections.singleton("Spacy"), true);
         outputStream.close();
+        outputXmlFile.delete();
     }
 }
